887c624525dc2a80d33d3ee23aefd0a0363fb160
seongzzang <87599027+seongzzang@users.noreply.github.com>
2022-10-17 20:32:31 +0900

    Initial Commit

---
+++ Notice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Notice
+//
+//  Created by 양성혜 on 2022/10/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
